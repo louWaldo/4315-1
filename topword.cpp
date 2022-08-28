e85a160922e10a46e06029cbcc90d7b9dd50b742
Kevin <floreskevin51398@gmail.com>
2022-08-28 12:33:26 -0500

    getting the map set up and parsing it

--- topword.cpp
+++ topword.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
 using namespace std;
             // "input=f.txt;output=out.txt"
+
+template<typename K, typename V>
+void print_map(std::unordered_map<K, V> const &m)
+{
+    for (auto it = m.cbegin(); it != m.cend(); ++it) {
+        std::cout << "{" << (*it).first << ": " << (*it).second << "}\n";
+    }
+}
+
+
 int main(int argc, char** argv) {
     string outfile = argv[1];
     string delimiter = ";";
+    unordered_map<string, int> umap;
 
     size_t pos = 0;
     string infile;
@@ -15,7 +27,8 @@ int main(int argc, char** argv) {
         outfile.erase(0, pos + delimiter.length());
     }
     string ot = outfile.substr(7);
-
+//     Your program should display a small “help” if not input parameters are provided and must write error messages to the screen. Your program should not crash, halt unexpectedly or produce unhandled
+// exceptions. Consider empty input, blank lines and so on.
     string Text;
     ofstream out(ot);
     ifstream inf(outstring);
@@ -23,16 +36,34 @@ int main(int argc, char** argv) {
     while (getline (inf, Text)) {
     // Output the text from the file
         if (Text.length() > 1) {
-            out << Text << endl;
+            // out << Text << endl;
 
-        }    
+            // split each Text into words and put them into the unordered_map
+            string word;
+            size_t pos = 0;
+            while ((pos = Text.find(" ")) != std::string::npos) {
+                word = Text.substr(0, pos);
+                Text.erase(0, pos + 1);
+                // out << word << endl;
+                if (umap.find(word) == umap.end()) {
+                    umap[word] = 1;
+                } else {
+                    umap[word]++;
+                }
+            }
+            if (umap.find(Text) == umap.end()) {
+                umap[Text] = 1;
+            } else {
+                umap[Text]++;
+            }
 
+            print_map(umap);
+            cout << umap.size() << endl;
+        }
+            // clean up logic for text : know rules but your program should handle strange symbols, long/empty lines, and so on.
+            // Numbers can be ignored.
 
-
-
-     cout << Text;
-}
-
+    }
 
 
 
