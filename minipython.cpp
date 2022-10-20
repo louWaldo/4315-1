206503a62d07094ebe6f85b371586f508341211f
Kevin <floreskevin51398@gmail.com>
2022-10-20 13:21:45 -0500

    This can open up and cout the python file, now to do the rest

--- minipython.cpp
+++ minipython.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -8,9 +9,34 @@ using namespace std;
 
 //  How to run it ./minipython freqnumber.py
 int main(int argc, char** argv){
-	cout << "You have entered " << argc << " arguments:" << "\n";
 
-	for (int i = 0; i < argc; ++i)
-		cout << argv[i] << "\n";
-	return 0;
+
+	try
+    {
+        // get the arguments
+        if (argc < 2) {
+            throw argc;
+        } 
+        else {
+            // Parsing arguments and getting the input and output file names
+            string pyfile = argv[1];
+
+			// Starting to parse input file and scrubb
+			string Text;
+			ifstream inf(pyfile);
+			while (getline(inf, Text)) {
+				cout << Text << endl;
+			}
+			inf.close();
+			cout << "You have entered " << pyfile << " to be parsed" << "\n";
+			return 0;
+		}
+		
+		
+	}
+    catch(int arguments)
+    {
+        std::cerr << "Please Give Correct Argument" << '\n' << "Example: ./topText 'input=filename.txt;output=outfile.txt'" << '\n';
+    }
+    return 0;
 }
